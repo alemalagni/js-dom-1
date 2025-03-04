3069d975a74dc168693f3bca0f77344033f79d3f
alemalagni <alemalagni@gmail.com>
2025-03-04 16:51:25 +0100

    feat: add JS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const element = document.getElementById("image");
 
-const button = document.querySelector('button',
-    
-)
+const button = document.getElementById("button");
+
+button.addEventListener("click" ,
+    function() {
+        console.log("Tasto premuto!")
+        //element.src = "img/yellow_lamp.png";
+    }
+);
