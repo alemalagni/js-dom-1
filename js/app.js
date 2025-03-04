ad1125363c9de8e6239dea87330338adea0ceac4
alemalagni <alemalagni@gmail.com>
2025-03-04 17:10:17 +0100

    feat: add JS structure bonus

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
+// Dichiarazioni variabili
 const element = document.getElementById("image");
-
 const button = document.querySelector('button');
+const text = document.getElementById("text");
+let acceso = false;
 
+// Script
 button.addEventListener('click',
     function() {
-        element.src = "img/yellow_lamp.png";
+        if (acceso === false) {
+            element.src = "img/yellow_lamp.png";
+            text.innerHTML = "Spegni"
+            acceso = true;
+        } else if (acceso === true) {
+            element.src = "img/white_lamp.png";
+            text.innerHTML = "Accendi"
+            acceso = false;
+        }
     }
 );
